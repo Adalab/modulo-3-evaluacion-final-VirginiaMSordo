3502c1f9a4a23f6a44f989b7343db7c99bcb432d
VirginiaMSordo <vickysalas23@gmail.com>
2022-12-14 22:27:32 +0100

    ReadMe

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,11 +1,15 @@
 import '../styles/pages/NotFound.scss';
 import image from '../images/morty.jpg';
+import { Link } from 'react-router-dom';
 
 function NotFound() {
     return <div className='error_message'>
         <p className="item1">Sorry, character not found, keep your eyes open.</p>
+        <p className="item1"> Reset to Go Back</p>
         <img className="found" src={image} alt="notFound" />
+
     </div>;
+
 }
 
 export default NotFound;
